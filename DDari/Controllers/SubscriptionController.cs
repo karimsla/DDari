15250d11c08aff7bb15424f80773c5e85cb8833d
dorra <dorra.kerro@gmail.com>
2021-04-21 00:38:37 +0100

    first

---
+++ DDari/Controllers/SubscriptionController.cs
@@ -0,0 +1,108 @@
+﻿using DDari.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDari.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+
+        static HttpClient client = null;
+        // GET: Subscription
+        public ActionResult Index()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8081");
+           // client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("/api/subscription").Result;
+            if (response.IsSuccessStatusCode)
+            {
+              //  ViewBag.result = response.Content.ReadAsStringAsync(IEnumerable<Subscription>).Result;
+            }
+            else
+            {
+                ViewBag.result("error");
+            }
+            return View();
+        }
+
+        // GET: Subscription/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Subscription/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subscription/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subscription/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Subscription/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Subscription/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Subscription/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
