6a4854571d0cff1af8df977b58bf2cf1384ccded
beheddine <beheddine.akermi@esprit.tn>
2021-04-21 20:10:41 +0100

    user entity

---
+++ DDari/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDari.Models
+{
+    public class Role
+    {
+        private long roleId { get; set; }
+        private String name { get; set; }
+
+
+    }
+}
---
+++ DDari/Models/Utilisateur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDari.Models
+{
+    public class Utilisateur
+    {
+        public string username { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public DateTime created { get; set; }
+        private bool enabled { get; set; }
+        private String picture { get; set; }
+        private String phone { get; set; }
+        private String resetPasswordToken { get; set; }
+
+    }
+}
