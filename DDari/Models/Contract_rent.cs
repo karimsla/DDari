d581257db5753bd4f0bf9e6b0c874f4bdb354f9f
mahdikhalil <mahdi.khalil@esprit.tn>
2021-04-22 11:01:31 +0100

    add prop

--- DDari/Models/Contract_rent.cs
+++ DDari/Models/Contract_rent.cs
@@ -7,5 +7,13 @@ namespace DDari.Models
 {
     public class Contract_rent
     {
+        public int id_user { get; set; }
+        public int id_property { get; set; }
+        
+        public DateTime dateDebut { get; set; }
+        public DateTime dateFin { get; set; }
+        public string details { get; set; }
+        public bool rented { get; set; }
+
     }
 }
